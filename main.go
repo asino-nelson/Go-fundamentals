b147fcca597e07b00a5810c76894438f9d63c4e0
asino-nelson <asinonelson276@gmail.com>
2024-06-24 15:10:21 +0300

    Arrays

--- main.go
+++ main.go
@@ -3,41 +3,65 @@ import "fmt"
 
 
 func main (){
-  // fmt.Println("Hello world")
 
-  // var intNum int32 = 100
-  // fmt.Println(intNum)
+  // DATA TYPES AND VARIABLES
 
-  // var floatNum float64 = 100.0
-  // fmt.Println(floatNum)
+  /*
+  fmt.Println("Hello world")
 
-  // var intNum1 int = 3
-  // var intNum2 int = 2
-  // fmt.Println(intNum1 + intNum2)
-  // fmt.Println(intNum1 - intNum2)
-  // fmt.Println(intNum1 * intNum2)
-  // fmt.Println(intNum1 / intNum2)
-  // fmt.Println(intNum1 % intNum2)
+  var intNum int32 = 100
+  fmt.Println(intNum)
 
+  var floatNum float64 = 100.0
+  fmt.Println(floatNum)
 
-  // var myString string = "Hello Nelson"
-  // fmt.Println(myString)
+  var intNum1 int = 3
+  var intNum2 int = 2
+  fmt.Println(intNum1 + intNum2)
+  fmt.Println(intNum1 - intNum2)
+  fmt.Println(intNum1 * intNum2)
+  fmt.Println(intNum1 / intNum2)
+  fmt.Println(intNum1 % intNum2)
 
-  // var myBool bool = true
-  // fmt.Println(myBool)
+  var myString string = "Hello Nelson"
+  fmt.Println(myString)
 
-  // myVar := "Milk"
-  // fmt.Println(myVar)
+  var myBool bool = true
+  fmt.Println(myBool)
 
+  myVar := "Milk"
+  fmt.Println(myVar)
+  */
+
+  // FUNCTIONS
+
+  /*
   sentence := "This is the print value"
 
   PrintMe(sentence)
   answer, remainder := intDivision(100,9)
   fmt.Println(answer)
   fmt.Printf("The result is %v with remainder %v", answer, remainder)
+*/
+
+// ARRAYS
+
+var intArr = [3] int {1,2,3}
+fmt.Println(intArr[0])
+fmt.Println(intArr[1:3])
+fmt.Println(intArr)
+
+fruits := [...]string{"Apple","Mango","Kiwi"}
+fmt.Println(fruits)
+
+
 
 }
 
+
+// FUNCTIONS
+
+/*
 func PrintMe(printValue string){
   fmt.Println(printValue)
 }
@@ -47,3 +71,4 @@ func intDivision(numerator int, denominator int)(int, int){
   remainder := numerator % denominator
   return result, remainder
 }
+*/
