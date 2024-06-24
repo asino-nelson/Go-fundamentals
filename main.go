0b0ab47610d5771a8078d96ea659b72a0b8b093f
asino-nelson <asinonelson276@gmail.com>
2024-06-24 15:28:27 +0300

    Slices

--- main.go
+++ main.go
@@ -45,14 +45,32 @@ func main (){
 */
 
 // ARRAYS
+/*
+  var intArr = [3] int {1,2,3}
+  fmt.Println(intArr[0])
+  fmt.Println(intArr[1:3])
+  fmt.Println(intArr)
 
-var intArr = [3] int {1,2,3}
-fmt.Println(intArr[0])
-fmt.Println(intArr[1:3])
-fmt.Println(intArr)
+  fruits := [...]string{"Apple","Mango","Kiwi"}
+  fmt.Println(fruits)
+*/
 
-fruits := [...]string{"Apple","Mango","Kiwi"}
-fmt.Println(fruits)
+// SLICES
+
+  var intSlice []int = []int{4,5,6}
+  fmt.Println(intSlice)
+  fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
+
+  intSlice = append(intSlice,7)
+  fmt.Println(intSlice)
+  fmt.Printf("\nThe length is %v with capacity %v \n", len(intSlice), cap(intSlice))
+
+  var intSlice2 []int = []int{8,9}
+  intSlice = append(intSlice, intSlice2 ...)
+  fmt.Println(intSlice)
+
+  var intSlice3 []int = make([]int, 3,8) // Weve specified length 3 and capcity 8
+  fmt.Println(intSlice3)
 
 
 
