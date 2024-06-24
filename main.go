366a8d739143c287bf07ef17cf26161a216c84f4
asino-nelson <asinonelson276@gmail.com>
2024-06-24 13:30:36 +0300

    Go variables and data types

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+
+func main (){
+  fmt.Println("Hello world")
+
+  var intNum int32 = 100
+  fmt.Println(intNum)
+
+  var floatNum float64 = 100.0
+  fmt.Println(floatNum)
+
+  var intNum1 int = 3
+  var intNum2 int = 2
+  fmt.Println(intNum1 + intNum2)
+  fmt.Println(intNum1 - intNum2)
+  fmt.Println(intNum1 * intNum2)
+  fmt.Println(intNum1 / intNum2)
+  fmt.Println(intNum1 % intNum2)
+
+
+  var myString string = "Hello Nelson"
+  fmt.Println(myString)
+
+  var myBool bool = true
+  fmt.Println(myBool)
+
+  myVar := "Milk"
+  fmt.Println(myVar)
+
+  
+
+}
